e000c6d4269238144c0d016a4fb383a53822973b
webdevopsnautone <100461356+webdevopsnautone@users.noreply.github.com>
2022-04-05 12:03:22 +0530

    countdown script done

--- script.js
+++ script.js
@@ -10,6 +10,40 @@ function closeNav() {
   document.getElementById("mySidenav").style.opacity = "0"; 
 }
 
+//------------------------------for numbersgrowth----------------------------------------//
+var a = 0;
+$(window).scroll(function() {
+
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $('.counter-value').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    a = 1;
+  }
+
+});
+
 
 //--------------------Main navbar transition---------------//
 
